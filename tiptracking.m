57335a348d87e2d0f1df2f3fde907855dbad4008
nanluo1987 <nan.luo@duke.edu>
2021-08-02 17:29:29 +0800

    Keep record of tip trajectories
    Store the coordinates of tips at every branch-updating step

---
+++ tiptracking.m
@@ -0,0 +1,6 @@
+function [xm2, ym2] = tiptracking(xm1, ym1, xm2, ym2)
+
+% input:  coordinates of branch tips of species 1 (at ib) & 2 (at ib - 1)
+% output: coordinates of branch tips of species 2 (at ib)
+% xm1,xm2,ym1,ym2 are matrices (number of tips x number of time points)
+
